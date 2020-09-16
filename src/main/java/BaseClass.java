0687f1a34f5a4674af1840475aea5ee847328baa
rajasekhar <rpasup@wipro.com>
2020-09-16 18:57:51 +0400

    updated one

--- src/main/java/BaseClass.java
+++ src/main/java/BaseClass.java
@@ -1,7 +1,10 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+
 
 public class BaseClass {
     public String projectUrl ="https://www.google.com/";
@@ -10,10 +13,16 @@ public class BaseClass {
     @BeforeClass
     public void setup(){
        try {
-        System.setProperty("webdriver.gecko.driver",System.getProperty("user.dir")+"//Driver//geckodriver");
-        options = Options();
-        options.headless = True;
+        // System.setProperty("webdriver.gecko.driver",System.getProperty("user.dir")+"//Driver//geckodriver");
+        // options = Options();
+        // options.headless = True;
         // driver = webdriver.Firefox(options=options, executable_path=r'C:\Utility\BrowserDrivers\geckodriver.exe')
+        FirefoxBinary firefoxBinary = new FirefoxBinary();
+        firefoxBinary.addCommandLineOptions("--headless");
+        System.setProperty("webdriver.gecko.driver",System.getProperty("user.dir")+"//Driver//geckodriver");
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.setBinary(firefoxBinary);
+        driver = new FirefoxDriver(firefoxOptions);
        } catch (Exception e) {
            //TODO: handle exception
            System.out.println(e.getLocalizedMessage());
